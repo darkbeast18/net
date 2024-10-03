71d2c92560a28abafe90f2115a398ec0b5a75bff
darkbeast18 <prudhvichowdary45436@gmail.com>
2024-10-03 05:45:21 +0530

    Add files via upload

---
+++ exp10/exp10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First string : ");
+
+            String s1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second string: ");
+             String s2 = Console.ReadLine();
+
+            Console.WriteLine(s1.ToUpper());
+
+            Console.WriteLine(s2.ToLower());
+
+            Console.ReadLine();
+        }
+    }
+}
